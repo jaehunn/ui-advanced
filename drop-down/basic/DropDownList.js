a43c6373a29f9ac435bdda46e64c9c184df9473a
bangjaehun <qkdwogns98@gmail.com>
2021-06-08 13:29:26 +0900

    ...

--- drop-down/basic/DropDownList.js
+++ drop-down/basic/DropDownList.js
@@ -1,58 +1,119 @@
-// @see https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
-
-// DropDownList 클래스
-
 export default class DropDownList {
-  constructor(config) {
-    this.rowHeight = 27;
-    this.emptyLabel = "선택해주세요."; // option 의 default label
-    this.currentIndex = -1; // null 대신 -1 로 초기설정
+  constructor({ selector, idField, labelField, data, changeEventHandler}) {
+    this.targetIndex = -1; // 선택된 item
 
     // 설정값 인자로 커스터마이징될 수 있다.
-    this.idField = config.idField || "id"; //
-    this.labelField = config.labelField || "label";
-    this.data = config.data;
-    this.callback = config.changeEvent;
+    this.idField = idField || "id"; //
+    this.labelField = labelField || "label";
+    this.data = data;
+    this.callback = changeEventHandler;
 
-    this.dropDownLabel = this.initialize(document.querySelector(config.selector), this.emptyLabel);
+    const dropDownContainerEl = document.querySelector(selector);
+    this.dropDownSelectLabelContainerEl = this.initialize(dropDownContainerEl);
 
-    // // option 이 선택되면 backdrop 에 표시한다.
-    // this.backDrop = document.querySelector(config.backDrop);
-    // this.dropDownItem = this.displayDropDownItemList(this.backDrop, config.data);
+    // option 이 선택되면 backdrop 에 표시한다.
+    this.backDropContainerEl = document.querySelector(backDrop);
+    this.dropDownItemEl = this.displayDropDownItemList(this.backDropContainerEl, data);
 
-    // // 마지막은 event bind
-    // this.eventBinding();
+    this.eventBinding();
   }
 
   // 초기 동작, 마운트 시 option 들을 렌더링한다.
-  initialize(selector, emptyLabel) {
-    // wrapper
-    let dropDownLabel = document.createElement("div");
-    dropDownLabel.classList.add("dropdown-select-label-container");
+  initialize(dropDownContainerEl, label = "선택해주세요.") {
+    const dropDownSelectLabelContainerEl = document.createElement("div");
+    dropDownSelectLabelContainerEl.classList.add("dropdown-select-label-container");
 
-    const renderText = `
-      <span class="dropdown-select-label">${emptyLabel}</span>
+    const dropDownSelectLabelHTML = `
+      <span class="dropdown-select-label">${label}</span>
       <div class="dropdown-select-arrow-container">
         <div class="dropdown-select-arrow"></div>
       </div>
     `;
 
     // 요소 시작 부분(begin) 안쪽(after)
-    dropDownLabel.insertAdjacentHTML("afterbegin", renderText);
+    dropDownSelectLabelContainerEl.insertAdjacentHTML("afterbegin", dropDownSelectLabelHTML);
 
-    // wrapper 통째를 자식으로 붙히기
-    selector.appendChild(dropDownLabel);
-
-    return dropDownLabel;
+    dropDownContainerEl.appendChild(dropDownSelectLabelContainerEl);
+    
+    return dropDownSelectLabelContainerEl;
   }
 
-  // selector box 의 option 들을 렌더링하는 메서드
-  displayDropDownItemList(selector, data) {
-    // ...
+  displayDropDownItemList(backDropContainerEl, data) {
+    if (!backDropContainerEl) return;
+
+    backDropContainerEl.innerHTML = data.reduce((result, item) => {
+      return result += 
+      `<div class="dropdown-item">
+        <span>${item[this.labelField]}</span>
+      </div>`
+    }, '<div class="dropdown-list">') + "</div>";
+
+    // 요소 좌표 가져오기
+    // @see https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
+    const dropDownSelectLabelContainerRect = this.dropDownSelectLabelContainerEl.getBoundingClientRect();
+    const dropDownListEl = document.querySelector(".dropdown-list");
+
+    const { width, top, left} = dropDownSelectLabelContainerRect;
+
+    dropDownListEl.style.cssText = `
+      position: absolute;
+      width: ${width}px;
+      top: ${top + 5}px;
+      left: ${left}px;
+    `
   }
 
-  // 요소에 이벤트 핸들러를 부착하는 메서드
   eventBinding() {
-    // ...
+    // 1. backDrop 클릭 이벤트 -> backDrop 숨기기
+    this.backDropContainerEl.addEventListener('click', () => {
+      this.backDropContainerEl.style.cssText = "display: none;";
+    })
+
+    // 2. dropdown label 클릭 이벤트 -> backDrop 나타내기
+    this.dropDownSelectLabelContainerEl.addEventListener("click", () => {
+      this.backDropContainerEl.style.cssText = "display: block;";
+    })
+
+    // 3. dropdown item 을 돌면서 
+    document.querySelectorAll("dropdown-item").forEach((dropDownItemEl, index) => {
+      dropDownItemEl.addEventListener("click", () => {
+        const currentOption = this.retrieveOptionByIndex(index);
+        
+        // remove active
+        if (~this.targetIndex) this.unselectedDropDownItem(this.targetIndex)
+
+        // add active
+        if (~this.targetIndex) this.selectedDropDownItem(this.targetIndex);
+
+        // dispatch 이용하기
+        this.dispatchEvent({
+          id: currentOption[this.idField],
+          label: currentOption[this.labelField]
+        })
+      })
+    })
+  }
+
+  // utils
+  dispatchEvent(payload) {
+    this.dropDownSelectLabelContainerEl.querySelector(".dropdown-select-label").innerHTML = payload[this.labelField];
+    this.backDropContainerEl.style.cssText = "display: none;";
+
+    // changeEvent 
+    this.callback(payload);
+  }
+
+  selectedDropDownItem(index) {
+    document.querySelectAll(".dropdown-item")[index].classList.add("selected");
+  }
+
+  unSelectedDropDownItem(index) {
+    document.querySelectAll(".dropdown-item")[index].classList.remove("selected");
+  }
+
+  retrieveOptionByIndex(index) {
+    const targetOption = this.data[index];
+
+    return targetOption;
   }
 }
--- drop-down/basic/script.js
+++ drop-down/basic/script.js
@@ -5,7 +5,10 @@ import DropDownList from "./DropDownList";
 main();
 
 function main() {
-  const dropDownList = new DropDownList({
+  const userListEl = document.querySelector("#userlist");
+  const defaultUserData = retrieveUserData();
+
+  new DropDownList({
     selector: "#dropdown",
     backdrop: ".back-drop",
     idField: "id",
@@ -15,20 +18,23 @@ function main() {
     // changeEvent 는 selector box 가 선택되면 동작한다.
     //  1. default 로 명시된 label 의 경우에는 모든 리스트를 뿌려준다.
     //  2. 특정 id 에 대하여 뿌려준다.
-    changeEvent: (e) => {
-      displayUserList(document.querySelector("#userlist"), e.id ? displayUserList().filter((item) => item.favorites === e.id) : displayUserList());
+    changeEventHandler: ({ id }) => {
+      const userData = id ? defaultUserData.filter(({ favorites }) => favorites === id) : defaultUserData;
+
+      renderUserList(userListEl, userData);
     },
   });
 
-  displayUserList(document.querySelector("#userlist"), retrieveUserData());
+  renderUserList(userListEl, defaultUserData);
 }
 
 // user 의 favorite 과 option 의 id 가 관계에 놓여있다.
-function displayUserList(selector, data) {
-  selector.innerHTML = data.reduce((result, item) => {
-    return (result += `<div>
-      <span>${item.userName}</span>
-      <span>favorites: ${retrieveOptionItemById(item["favorites"]).label}</span>
-    </div>`);
+function renderUserList(targetEl, data) {
+  targetEl.innerHTML = data.reduce((result, item) => {
+    return result +=
+      `<div>
+        <span>${item.userName}</span>
+        <span>favorites: ${retrieveOptionItemById(item["favorites"]).label}</span>
+      </div>`;
   }, "");
 }
